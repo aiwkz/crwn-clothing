7c53e9ed5301003318dbc262fb5a9e47567ea579
aiwkz <federicoj.pp@gmail.com>
2023-06-12 20:50:04 +0200

    🚧 adds sign-up component

--- src/routes/sign-in/sign-in.componet.jsx
+++ src/routes/sign-in/sign-in.componet.jsx
@@ -1,15 +1,20 @@
-import { signInWithGooglePopup, createUserFromAuth } from '../../utils/firebase/firebase.utils';
+import { 
+    signInWithGooglePopup,
+    createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserFromAuth(user)
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
 
     return (
         <div>
             <h1>SignIn page</h1>
-            <button onClick={logGoogleUser}>Log in with Google</button>
+            <button onClick={logGoogleUser} >Log in with Google Popup</button>
+            <SignUpForm />
         </div>
     );
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { 
-    getAuth, 
-    signInWithRedirect, 
+    getAuth,
     signInWithPopup, 
     GoogleAuthProvider 
 } from 'firebase/auth';
@@ -23,15 +22,15 @@ const firebaseConfig = {
   };
   
   const firebaseApp = initializeApp(firebaseConfig);
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
+  const googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({
     prompt: "select_account"
   });
 
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
   export const db = getFirestore();
-  export const createUserFromAuth = async (userAuth) => {
+  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+  export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
 
