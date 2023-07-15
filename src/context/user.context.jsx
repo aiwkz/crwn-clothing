1703d35efb0b61d43c82eec96fdcae5532c12842
aiwkz <federicoj.pp@gmail.com>
2023-07-15 14:30:46 +0200

    ✨ adds redux flow to cart and user context

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,21 +1,51 @@
 import {
     createContext,
-    useState,
-    useEffect
+    useEffect,
+    useReducer
 } from 'react';
 
 import {
     onAuthStateChangedListener,
     createUserDocumentFromAuth
 } from '../utils/firebase/firebase.utils';
+import { creatAction } from '../utils/reducer/reducer.utils.js';
 
 export const UserContext = createContext({
     currentUser: null,
     setCurrentUSer: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default: {
+            throw new Error(`Unhandled type ${type} in userReducer`);
+        }
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUSer] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    const { currentUser } = state;
+
+    const setCurrentUSer = (user) => {
+        dispatch(creatAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
+    }
+
     const value = { currentUser, setCurrentUSer }
 
     useEffect(() =>
