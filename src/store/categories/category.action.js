e9d9b9c2006e0bff077502b0a9407b6770634390
aiwkz <federicoj.pp@gmail.com>
2024-02-04 18:06:52 +0100

    ➕ Adds and implements Redux Thunk

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,22 @@
 
 import { CATEGORIES_ACTION_TYPE } from './category.types';
-import { creatAction } from '../../utils/reducer/reducer.utils';
 
-export const setCategories = (categories) => creatAction(CATEGORIES_ACTION_TYPE.SET_CATEGORIES, categories);
+import { creatAction } from '../../utils/reducer/reducer.utils';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+export const fetchCategoriesStart = () => creatAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSucceeded = (categories) => creatAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCEEDED, categories);
+
+export const fetchCategoriesFailed = (error) => creatAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+
+  try {
+    const categoriesArray = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSucceeded(categoriesArray));
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error));
+  }
+};
--- src/store/store.js
+++ src/store/store.js
@@ -2,16 +2,17 @@ import { compose, legacy_createStore as createStore, applyMiddleware } from 'red
 import logger from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import { thunk } from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'],
+  whiteList: ['cart'],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 const composedEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
